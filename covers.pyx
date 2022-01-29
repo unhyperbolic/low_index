ae15aed26053cec4ba1f2ff0c95e4a64fcb9d32d
Marc Culler <culler@users.noreply.github.com>
2022-01-28 22:37:58 -0600

    Add an iterator for reduced words in the (length, lex) order.

--- covers.pyx
+++ covers.pyx
@@ -50,30 +50,33 @@ Conventions:
 
 
 cdef class cWord():
-    cdef int rank, start, max, length
+    cdef int rank, start, length, size
     cdef char* buffer
 
     def __cinit__(self, int rank, byteseq):
         self.rank = rank
-        self.start = 0
-        self.max = self.length = len(byteseq)
-        self.buffer = <char *>PyMem_Malloc(self.max)
+        self.start = 1
+        self.length = len(byteseq)
+        self.size = self.length + 2
+        self.buffer = <char *>PyMem_Malloc(self.size)
 
     def __init__(self, int rank, byteseq):
-        for n, x in enumerate(byteseq):
-            if x == 0 or x < -self.rank or x > self.rank:
+        cdef int x  # GRRR C allows negative chars but Cython doesn't.
+        for n in range(len(byteseq)):
+            x = byteseq[n]
+            x = x if x < 128 else x - 256  # GRRR No negative chars in Cython.
+            if x == 0 or x > self.rank or x < -self.rank :
                 raise ValueError(
-                    'Letters are non-zero and in [-{},{}]'.format(self.rank)
+                    'Invalid letter {0}; must be non-zero and in [-{1},{1}]'.format(
+                        x, self.rank)
                     )
-            self.buffer[n] = x
+            self.buffer[self.start + n] = x
 
     def __dealloc__(self):
         PyMem_Free(self.buffer)
 
     def __len__(self):
-        if self.length > 0:
-            return self.length
-        return 0
+        return self.length
 
     def __repr__(self):
         return 'cWord(%s)'%str(self)
@@ -86,11 +89,12 @@ cdef class cWord():
 
     cdef repack(self):
         cdef char *old = self.buffer
-        self.max = self.length
-        self.buffer = <char *>PyMem_Malloc(self.max)
+        self.size = self.length + 2
+        self.buffer = <char *>PyMem_Malloc(self.size)
+        self.buffer[0] = 0
         for i in range(self.length):
             self.buffer[i] = old[self.start + i]
-        self.start = 0
+        self.start = 1
         PyMem_Free(old)
         
     cdef reduce(self):
@@ -131,11 +135,102 @@ cdef class ReducedWord(cWord):
         cWord.__init__(self, rank, byteseq)
         cWord.reduce(self)
 
+    cdef _next_letter(self, int n):
+        if n > 0:
+            return -n
+        if n > -self.rank:
+            return 1 - n
+        return 1
+
+    cpdef _increment(self):
+        """
+        Private method!  Rewrites the buffer with the next word in the
+        (length, lex) ordering, reallocating memory if necessary.
+        [[This is harder that one would expect]].
+        """
+        cdef char next
+        cdef char* buf = &self.buffer[self.start]
+        cdef int len = self.length
+        # Special case for the trivial word
+        if len == 0:
+            buf[0] = 1
+            self.length = 1
+            return
+        # Easy case; just increase the last letter.
+        if buf[len - 1] + self.rank != 0:
+            next = self._next_letter(buf[len - 1])
+            # But do not create a cancellation
+            if len > 1 and (next + buf[len - 2] == 0):
+                next = self._next_letter(next)
+            if next != 1:
+                buf[len - 1] = next
+                return
+        # Backtracking in the Cayley graph of the free group.
+        n = len - 1
+        # The word ends in a power of the next to biggest generator.
+        if buf[n] == self.rank:
+            while n > 0 and buf[n - 1] == self.rank:
+                buf[n] = 1
+                n -= 1
+            buf[n] = -self.rank
+        # The word ends in a power of the biggest generator.
+        elif buf[n] + self.rank == 0:
+            while buf[n] + self.rank == 0:
+                n -= 1
+            if n >= 0:
+                buf[n] = self._next_letter(buf[n])
+                if n > 0 and buf[n-1] + buf[n] == 0:
+                    buf[n] = self._next_letter(buf[n])
+                if buf[n] + 1 == 0:
+                    for n in range(n + 1, self.length):
+                        buf[n] = -1
+                else:
+                    for n in range(n + 1, self.length):
+                        buf[n] = 1
+        if n < 0:
+            # We need to increase the length.
+            # The first word will be a power of the first generator.
+            if self.start < 1:
+                self.repack()
+            self.start -= 1
+            self.length += 1
+            buf = &self.buffer[self.start]
+            for i in range(self.length):
+                buf[i] = 1
+
+    def __copy__(self):
+        if self.length == 0:
+            return ReducedWord(self.rank, [])
+        return ReducedWord(self.rank,
+            self.buffer[self.start:self.start + self.length])
+
+cdef class ReducedWords:
+    """
+    Iterator for reduced words of a given rank with bounded length.
+    """
+    cdef ReducedWord current
+    cdef int rank, max_length
+
+    def __init__(self, int rank, int max_length=1):
+        self.rank = rank
+        self.max_length = max_length
+        self.current = ReducedWord(rank, [])
+
+    def __next__(self):
+        if self.current.length > self.max_length:
+            raise StopIteration
+        result = self.current.__copy__()
+        self.current._increment()
+        return result
+
+    def __iter__(self):
+        return self
+
 cdef class CyclicallyReducedWord(ReducedWord):
 
     def __init__(self, int rank, byteseq):
         ReducedWord.__init__(self, rank, byteseq)
-        cWord.cyclically_reduce(self)
+        cWord.cyclically_reduce(self)            
 
 cdef class CoveringSubgraph:
     cdef public int rank
