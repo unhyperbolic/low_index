b4251d723d16db23ce6053d4af8633cd5a779428
Nathan Dunfield <nathan@dunfield.info>
2022-03-12 13:47:56 -0600

    test: added verbose options and return code

--- python_src/test.py
+++ python_src/test.py
@@ -4,6 +4,8 @@ import low_index
 import types
 from doctest import DocTestFinder, DocTestParser, DocTestRunner
 
+modules = [low_index]
+
 def fix_module_doctest(module):
     """
     Extract docstrings from cython functions that would be skipped by doctest
@@ -20,12 +22,10 @@ def fix_module_doctest(module):
             pass
         if inspect.isclass(value) or inspect.isfunction(value):
             if value.__doc__ and isinstance(value.__doc__, str):
-                print('Including doctests for Cython class %s.'%name)
                 module.__test__[name] = value.__doc__
 
 def doctest_module(module, verbose=False):
     finder = doctest.DocTestFinder(parser=DocTestParser())
-    failed, attempted = 0, 0
     if isinstance(module, types.ModuleType):
         fix_module_doctest(module)
         runner = doctest.DocTestRunner(verbose=verbose)
@@ -34,12 +34,20 @@ def doctest_module(module, verbose=False):
         result = runner.summarize()
     else:
         result = module(verbose=verbose)
-    failed += result.failed
-    attempted += result.attempted
-    print(result)
+    return result
 
 def runtests(verbose=False):
-    doctest_module(low_index, verbose=verbose)
+    success = True
+    for module in modules:
+        result = doctest_module(low_index, verbose=verbose)
+        if result.failed > 0:
+            success = False
+        print(module.__name__ + ':', result)
+
+    return success
 
 if __name__ == '__main__':
-    runtests()
+    import sys
+    verbose = '-v' in sys.argv or '--verbose' in sys.argv
+    if not runtests(verbose):
+        sys.exit(1)
