871798f84e07f1aa0077510ab5d4b11fa94531b8
Marc Culler <culler@users.noreply.github.com>
2022-03-13 09:48:22 -0500

    Add a clean command to setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from setuptools import setup, Extension
-#from Cython.Build import cythonize
-import re
+from setuptools import setup, Command, Extension
 import os
 import sys
+import re
+import shutil
+from glob import glob
+
+class Clean(Command):
+    user_options = []
+    def initialize_options(self):
+        pass 
+    def finalize_options(self):
+        pass
+    def run(self):
+        for dir in ['build', 'dist', 'low_index.egg-info']:
+            shutil.rmtree(dir, ignore_errors=True)
+        for file in glob('cython_src/*.c'):
+            os.remove(file)
 
 if sys.platform.startswith('win'):
     extra_compile_args = ['/Ox']
@@ -12,8 +25,7 @@ else:
 low_index = Extension(
     name = 'low_index._low_index',
     sources = ['cython_src/_low_index.pyx'],
-    compiler_directives = {'language_level' : "3"},
     extra_compile_args = extra_compile_args
 )
 
-setup(ext_modules = [low_index])
+setup(ext_modules = [low_index], cmdclass = {'clean':Clean,})
