14f5984a1fda28340d192bd5d3e1e475b7fc3704
Marc Culler <culler@users.noreply.github.com>
2022-03-18 07:54:47 -0500

    Fix cpuinfo for Linux.

--- python_src/benchmark.py
+++ python_src/benchmark.py
@@ -74,9 +74,13 @@ def cpu_info():
     elif platform.system() == 'Darwin': 
         command = '/usr/sbin/sysctl -n machdep.cpu.brand_string' 
         return os.popen(command).read().strip() 
-    elif platform.system() == 'Linux': 
-        command = 'cat /proc/cpuinfo' 
-        return os.popen(command).read().strip() 
+    elif platform.system() == 'Linux':
+        with open('/proc/cpuinfo') as input_file:
+            info = input_file.read().split('\n')
+            for line in info:
+                parts = line.split(':')
+                if parts[0].strip() == 'model name':
+                    return parts[1].strip()
     return 'Unknown CPU' 
 
 def run(ex):
