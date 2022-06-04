084428871eebe1696860a3603120fed54fcda44b
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-06-04 11:30:16 -0500

    Fixing cpp files.

--- cpp_low_index/cpp_src/wrapCoveringSubgraph.cpp
+++ cpp_low_index/cpp_src/wrapCoveringSubgraph.cpp
@@ -3,6 +3,7 @@
 #include "pybind11/pybind11.h"
 
 void addCoveringSubgraph(pybind11::module_ &m) {
-    pybind11::class_<CoveringSubgraph>(m, "CoveringSubgraph", pybind11::init<int>())
+    pybind11::class_<CoveringSubgraph>(m, "CoveringSubgraph")
+        .def(pybind11::init<int>())
         .def("GetRank", &CoveringSubgraph::GetRank);
 }
