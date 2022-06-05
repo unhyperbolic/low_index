e2d51617bd0603e8eedeceaaa0c25c823f062951
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-06-05 09:46:08 -0500

    Fixing bug in relators_may_lift.

--- cpp_low_index/cpp_src/simsNode.cpp
+++ cpp_low_index/cpp_src/simsNode.cpp
@@ -65,6 +65,8 @@ SimsNode::relators_may_lift(SimsNode * child,
     for (size_t n = 0; n < relators.size(); n++) {
         for (unsigned int v = 0; v < child->degree; v++) {
 
+            std::cout << "Checking relator " << n << " for node " << v << std::endl;
+
             const size_t j = n * max_degree + v;
             
             DegreeType vertex = _lift_vertices[j];
@@ -82,9 +84,9 @@ SimsNode::relators_may_lift(SimsNode * child,
                 label = relators[n][i];
                 save = vertex;
                 if (label > 0) {
-                    vertex = outgoing[rank * (vertex - 1) + label - 1];
+                    vertex = child->outgoing[rank * (vertex - 1) + label - 1];
                 } else {
-                    vertex = incoming[rank * (vertex - 1) - label - 1];
+                    vertex = child->incoming[rank * (vertex - 1) - label - 1];
                 }
                 if (vertex == 0) {
                     child->_lift_vertices[j] = save;
@@ -93,9 +95,12 @@ SimsNode::relators_may_lift(SimsNode * child,
                 }
             }
 
+            std::cout << "Ending loop with i = " << i << std::endl;
+            
             if (i >= relators[n].size() - 1) {
                 if (vertex == 0) {
                     if (!child->verified_add_edge(label, save, v + 1)) {
+                        std::cout << "verified_add_edge false" << std::endl;
                         return false;
                     }
                     if (child->is_complete()) {
@@ -107,6 +112,7 @@ SimsNode::relators_may_lift(SimsNode * child,
                     child->_lift_vertices[j] = 255;
                     child->_lift_indices[j] = relators[n].size();
                 } else {
+                    std::cout << "no false" << std::endl;
                     return false;
                 }
             }
