bcf8aaa169934c16997993b5b9d6cd3ba19e8e87
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-06-12 11:30:27 -0500

    Some more work on the integer types and moving num_relators to SimsNode.

--- cpp_low_index/cpp_src/coveringSubgraph.cpp
+++ cpp_low_index/cpp_src/coveringSubgraph.cpp
@@ -5,13 +5,11 @@
 
 CoveringSubgraph::CoveringSubgraph(
         const CoveringSubgraph::RankType rank,
-        const CoveringSubgraph::DegreeType max_degree,
-        const unsigned int num_relators)
+        const CoveringSubgraph::DegreeType max_degree)
   : rank(rank)
   , degree(1)
   , max_degree(max_degree)
   , num_edges(0)
-  , num_relators(num_relators)
   , outgoing(rank * max_degree, 0)
   , incoming(rank * max_degree, 0)
   , _slot_index(0)
--- cpp_low_index/cpp_src/simsNode.cpp
+++ cpp_low_index/cpp_src/simsNode.cpp
@@ -4,7 +4,8 @@ SimsNode::SimsNode(
         const CoveringSubgraph::RankType rank,
         const CoveringSubgraph::DegreeType max_degree,
         const unsigned int num_relators)
- : CoveringSubgraph(rank, max_degree, num_relators)
+ : CoveringSubgraph(rank, max_degree)
+ , num_relators(num_relators)
  , _lift_indices(num_relators * max_degree, 0)
  , _lift_vertices(num_relators * max_degree, 0)
 {
@@ -70,9 +71,9 @@ SimsNode::relators_may_lift(const std::vector<std::vector<int>> &relators)
             if (vertex == 0) {
                 vertex = v + 1;
             }
-            unsigned int i;
-            DegreeType index = _lift_indices[j];
-            DegreeType save;
+            RelatorLengthType i;
+            RelatorLengthType index = _lift_indices[j];
+            RelatorLengthType save;
             int label;
             for (i = index; i < relators[n].size(); i++) {
                 label = relators[n][i];
