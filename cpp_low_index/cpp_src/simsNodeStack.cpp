a7ebb12ed66471506ddbdcd0c145a1620a57ad5d
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-06-18 18:30:15 -0700

    Fixing a one off error when allocating SimsNodeStack.

--- cpp_low_index/cpp_src/simsNodeStack.cpp
+++ cpp_low_index/cpp_src/simsNodeStack.cpp
@@ -6,7 +6,7 @@ size_t
 SimsNodeStack::_compute_memory_size(const SimsNode &node)
 {
     const StackedSimsNode::_MemoryLayout layout(node);
-    return node.max_degree() * node.rank() * layout.size;
+    return (node.max_degree() * node.rank() + 1) * layout.size;
 }
 
 SimsNodeStack::SimsNodeStack(const SimsNode &node)
