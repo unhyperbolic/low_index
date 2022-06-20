7298deb880fc280c8d09664d43afc7845e7bce13
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-06-19 21:13:23 -0700

    Use setuptools to build to avoid pybind11 python package dependency.

--- cpp_low_index/setup.py
+++ cpp_low_index/setup.py
@@ -1,48 +1,52 @@
 import sys
+from setuptools import setup, Extension
 
-from pybind11 import get_cmake_dir
-# Available at setup time due to pyproject.toml
-from pybind11.setup_helpers import Pybind11Extension
-from setuptools import setup
+# Version 1.1 is used for the cython implemention
+# Version 1.2 is used for the C++/pybind11 implementation
+__version__ = "1.2"
 
-__version__ = "0.0.1"
+sources = [
+    "cpp_src/lowIndex.cpp",
+    "cpp_src/coveringSubgraph.cpp",
+    "cpp_src/simsNode.cpp",
+    "cpp_src/stackedSimsNode.cpp",
+    "cpp_src/abstractSimsNode.cpp",
+    "cpp_src/simsTree.cpp",
+    "cpp_src/wrapLowIndex.cpp",
+    "cpp_src/wrapCoveringSubgraph.cpp",
+    "cpp_src/wrapSimsNode.cpp",
+    "cpp_src/wrapAbstractSimsNode.cpp",
+    "cpp_src/wrapSimsTree.cpp",
+    "cpp_src/wrapModule.cpp"
+]
 
+if sys.platform.startswith('win'):
+    extra_compile_args = ['/Ox', '/std:c++11']
+else:
+    extra_compile_args = ['-O3', '-std=c++11']
 
 ext_modules = [
-    Pybind11Extension("cpp_low_index",
-        [
-            "cpp_src/lowIndex.cpp",
-            "cpp_src/coveringSubgraph.cpp",
-            "cpp_src/simsNode.cpp",
-            "cpp_src/stackedSimsNode.cpp",
-            "cpp_src/abstractSimsNode.cpp",
-            "cpp_src/simsTree.cpp",
-            "cpp_src/wrapLowIndex.cpp",
-            "cpp_src/wrapCoveringSubgraph.cpp",
-            "cpp_src/wrapSimsNode.cpp",
-            "cpp_src/wrapAbstractSimsNode.cpp",
-            "cpp_src/wrapSimsTree.cpp",
-            "cpp_src/wrapModule.cpp"
-         ],
-        cxx_std=11, # C++17 implies macOS 10.14
-        # Example: passing in the version to the compiled code
-        define_macros = [('VERSION_INFO', __version__)],
-        ),
+    Extension(
+        # Rename to low_index eventually, replacing the cython implementation
+        name = 'cpp_low_index',
+        sources = sources,
+        extra_compile_args = extra_compile_args)
 ]
 
 
 setup(
+    # Rename to low_index eventually, replacing the cython implementation
     name="cpp_low_index",
     version=__version__,
-    author="MMN",
-    author_email="",
+    author = 'Marc Culler and Nathan M. Dunfield and Matthias Goerner',
+    author_email = 'culler@uic.edu, nathan@dunfield.info, enischte@gmail.com',
     url="",
     description="A test project using pybind11",
     long_description="",
     ext_modules=ext_modules,
-    extras_require={"test": "pytest"},
+#    extras_require={"test": "pytest"},
     # Currently, build_ext only provides an optional "highest supported C++
     # level" feature, but in the future it may provide more features.
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.6"
 )
