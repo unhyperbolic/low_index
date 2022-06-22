6f551aa482a44106759e5aeaee157d71de04f5bc
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-06-22 15:19:37 -0700

    Adding documentation for SimsNode.

--- cpp_low_index/cpp_src/wrapSimsNode.cpp
+++ cpp_low_index/cpp_src/wrapSimsNode.cpp
@@ -1,4 +1,5 @@
 #include "simsNode.h"
+#include "docSimsNode.h"
 
 #include "pybind11/pybind11.h"
 
@@ -7,15 +8,18 @@
 namespace low_index {
 
 void addSimsNode(pybind11::module_ &m) {
-    pybind11::class_<SimsNode, AbstractSimsNode>(m, "SimsNode")
+    pybind11::class_<SimsNode, AbstractSimsNode>(
+                    m, "SimsNode",
+                    DOC(low_index, SimsNode))
         .def(pybind11::init<RankType,
                             DegreeType,
                             unsigned int>(),
              pybind11::arg("rank"),
              pybind11::arg("max_degree"),
              pybind11::arg("num_relators") = 0,
-             pybind11::doc(
-                 R"pbdoc(SimsNode is a partial covering subgraph with checks to see whether relators lift)pbdoc"));
+             DOC(low_index, SimsNode, SimsNode))
+        .def(pybind11::init<const SimsNode&>(),
+             DOC(low_index, SimsNode, SimsNode_3));
 }
 
 } // Namespace low_index
