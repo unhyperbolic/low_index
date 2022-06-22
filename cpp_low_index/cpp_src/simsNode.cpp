a37874945b8a0ab7d536761b014b6609c09853f1
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-06-22 16:41:37 -0700

    Adding more documentation.

--- cpp_low_index/cpp_src/simsNode.cpp
+++ cpp_low_index/cpp_src/simsNode.cpp
@@ -34,6 +34,7 @@ SimsNode::SimsNode(
 SimsNode::SimsNode(const SimsNode &other)
  : AbstractSimsNode(other)
 {
+    // Bail if invalid. This can happen if we moved from this SimsNode.
     if (!other._memory) {
         return;
     }
@@ -45,6 +46,9 @@ SimsNode::SimsNode(SimsNode &&other)
  : AbstractSimsNode(other)
  , _memory(std::move(other._memory))
 {
+    // std::move changed owner ship of the heap allocated memory to
+    // this node. Now we just need to point to the same memory than
+    // the other node did.
     _outgoing = other._outgoing;
     _incoming = other._incoming;
     _lift_indices = other._lift_indices;
