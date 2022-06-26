549e41270e8041c98b61c97d72f7f53ca317d700
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-06-26 11:26:00 -0700

    Adding missing file.

---
+++ cpp_low_index/cpp_src/wrapSimsTreeMultiThreaded.cpp
@@ -0,0 +1,24 @@
+#include "simsTreeMultiThreaded.h"
+
+#include "pybind11/pybind11.h"
+
+#include "pybind11/stl.h"
+
+namespace low_index {
+
+void addSimsTreeMultiThreaded(pybind11::module_ &m) {
+    pybind11::class_<SimsTreeMultiThreaded, SimsTreeBase>(
+            m, "SimsTreeMultiThreaded")
+        .def(pybind11::init<RankType,
+                            DegreeType,
+                            const std::vector<Relator> &,
+                            const std::vector<Relator> &,
+                            unsigned int>(),
+             pybind11::arg("rank"),
+             pybind11::arg("max_degree"),
+             pybind11::arg("short_relators"),
+             pybind11::arg("long_relators"),
+             pybind11::arg("thread_num"));
+}
+
+}
