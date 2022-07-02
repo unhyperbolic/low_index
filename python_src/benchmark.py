ff6cb6ae563d1a64891dfdc435974c6ae30decf3
Nathan Dunfield <nathan@dunfield.info>
2022-07-02 15:41:43 +0000

    Updated the Cython benchmarks to match those for C++

--- python_src/benchmark.py
+++ python_src/benchmark.py
@@ -3,88 +3,120 @@ import sys
 import platform
 import time
 from low_index import *
+
 examples = [
     {
         'group' : 'Fundamental group of K11n34',
         'rank': 3,
-        'relators': [
-            "aaBcbbcAc",
-            "aacAbCBBaCAAbbcBc"
-            ],
+        'short relators': ["aaBcbbcAc"],
+        'long relators': ["aacAbCBBaCAAbbcBc"],
         'index': 7,
-        'num_long': 1,
     },
     {
         'group' : 'Fundamental group of K11n34',
         'rank': 3,
-        'relators': [
-            "aaBcbbcAc",
-            "aacAbCBBaCAAbbcBc"
-            ],
+        'short relators': ["aaBcbbcAc"],
+        'long relators':  ["aacAbCBBaCAAbbcBc"],
         'index': 8,
-        'num_long': 1,
     },
     {
         'group' : 'Fundamental group of K15n12345',
         'rank': 3,
-        'relators': [
-            "aBcACAcb",
-            "aBaCacBAcAbaBabaCAcAbaBaCacBAcAbaBabCAcAbABaCabABAbABaCabCAcAb",
-            ],
+        'short relators': ["aBcACAcb"],
+        'long relators': ["aBaCacBAcAbaBabaCAcAbaBaCacBAcAbaBabCAcAbABaCabABAbABaCabCAcAb"],
         'index': 7,
-        'num_long': 1,
     },
     {
         'group' : 'Fundamental group of K15n12345',
         'rank': 3,
-        'relators': [
-            "aBcACAcb",
-            "aBaCacBAcAbaBabaCAcAbaBaCacBAcAbaBabCAcAbABaCabABAbABaCabCAcAb",
-            ],
+        'short relators': ["aBcACAcb"],
+        'long relators':["aBaCacBAcAbaBabaCAcAbaBaCacBAcAbaBabCAcAbABaCabABAbABaCabCAcAb"],
         'index': 8,
-        'num_long': 1,
     },
     {
         'group' : 'Fundamental group of o9_15405',
         'rank': 2,
-        'relators': [
-            "aaaaabbbaabbbaaaaabbbaabbbaaaaaBBBBBBBB"
-            ],
+        'short relators': ["aaaaabbbaabbbaaaaabbbaabbbaaaaaBBBBBBBB"],
+        'long relators': [],
         'index': 9,
-        'num_long': 0,
     },
     {
         'group' : 'Fundamental group of o9_15405',
         'rank': 2,
-        'relators': [
-            "aaaaabbbaabbbaaaaabbbaabbbaaaaaBBBBBBBB"
-            ],
+        'short relators': [],
+        'long relators': ["aaaaabbbaabbbaaaaabbbaabbbaaaaaBBBBBBBB"],
         'index': 9,
-        'num_long': 1,
     },
     {
-        'group' : 'Modular Group',
+        'group': 'K5_13(3,0)',
         'rank': 2,
-        'relators': [
-            "aa",
-            "bbb",
-            ],
-        'index': 25,
-        'num_long': 0,
+        'short relators': ['bbb', 'abABaaBAbabABaaBAbabABAb'],
+        'long relators': [],
+        'index': 15
+    },
+    {
+        'group': 'K15n145097(0,1)',
+        'rank': 3,
+        'short relators': ['aBCBabaBAbcbAccAbC'],  # Fastest was just one short rel
+        'long relators': ['aBAbcbAcbAcAbAcAbAcAbCaBC',
+                          'aBCBabABaaaaaaaaaaaaaaaaBAbcbAccbAcbAcAC'],
+        'index': 7,
+    },
+    {
+        'group': 'DodecahedralOrientableClosedCensus[3]',
+        'rank': 4,
+        'short relators': ['abbDDabacdc',
+                           'acdAcDc',
+                           'aBacddBBcD',
+                           'acddCdCbAddB'],
+        'long relators': [],
+        'index': 6,
+    },
+    {
+        'group': 'DodecahedralOrientableClosedCensus[8]',
+        'rank': 4,
+        'short relators': ['adADAddAbdbdbadaDD',
+                           'adaDDbcccDAddAbdb',
+                           'adaDDbccACACdaDDadCCBdAbcBdAbcBD',
+                           'acacaDAddADcacaCCBdAbcBdAbcBdAbc'],
+        'long relators': [],  # It didn't seem to help to make some rels long.
+        'index': 5,
+    },
+    {
+        'group': 'L14a26995(2,0)(2,0)(2,0)',
+        'rank': 4,
+        'short relators': ['bb',
+                           'aa',
+                           'cdcd',
+                           'aCABacAddcDbABdCDDaCAbac',
+                           'aBdcDbABddcDbaBdCDbABacAb',
+                           'aCAbacACBDCbcaCAbacACBcdbcaCABacACBc'],
+        'long relators': [], # Making rels long *slowed things down*.
+        'index': 7,
     },
     {
         'group' : 'Symmetric Group S7',
         'rank' : 2,
-        'relators': [
+        'short relators': [
             "aaaaaaa",
             "bb",
             "abababababab",
             "AbabAbabAbab",
             "AAbaabAAbaab",
             "AAAbaaabAAAbaaab"],
+        'long relators': [],
         'index': 35,
-        'num_long': 0,
-    }
+    },
+    # This example must go at the end to avoid an infinite loop when
+    # using Magma for the benchmark.
+    {
+        'group' : 'Modular Group',
+        'rank': 2,
+        'short relators': ["aa", "bbb"],
+        'long relators': [],
+        'index': 25,
+        'gap skip': True,
+    },
 ]
 
 def cpu_info(): 
@@ -102,17 +134,21 @@ def cpu_info():
                     return parts[1].strip()
     return 'Unknown CPU' 
 
-def run(ex):
+def run(ex, multi=True):
     print('%s; index = %d.'%(ex['group'], ex['index']))
-    t = SimsTree(ex['rank'], ex['index'], ex['relators'], num_long_relators=ex['num_long'])
+    relators = ex['short relators'] + ex['long relators']
+    t = SimsTree(ex['rank'], ex['index'], relators , num_long_relators=len(ex['long relators']))
     start = time.time()
-    x = t.list()
+    x = t.list(multi)
     elapsed = time.time() - start
     print('%d subgroups'%len(x))
-    print('%.3fs'%elapsed)
+    print('%.3fs\n'%elapsed)
     sys.stdout.flush()
 
 if __name__ == '__main__':
     print(cpu_info(), 'with', os.cpu_count(), 'cores')
+    multi = not '-single' in sys.argv
+    if not multi:
+        print('Running single-threaded')
     for example in examples:
-        run(example)
+        run(example, multi)
