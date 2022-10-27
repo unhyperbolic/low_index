fa34859e8d168cfd271a6ddbc477d99f3eb64f12
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-10-27 15:30:24 -0700

    Aligning Relator with signatures of std::vector and with the coding style.

--- cpp_low_index/cpp_src/relator.cpp
+++ cpp_low_index/cpp_src/relator.cpp
@@ -2,45 +2,45 @@
 
 namespace low_index {
 
-  void Relator::push_back(LetterType x) {
-    letters.push_back(x);
-  }
-  size_t Relator::size() {
-    return letters.size();
-  }
-  size_t Relator::size() const {
-    return letters.size();
-  }
-  void Relator::reserve(size_t n) {
-    letters.reserve(n);
-  }
-  LetterType Relator::back() {
-    return letters.back();
-  }
-  LetterType Relator::back() const {
-    return letters.back();
-  }
-  LetterType Relator::operator[](int index) {
-    return letters[index];
-  }
-  LetterType Relator::operator[](int index) const {
-    return letters[index];
-  }
-  bool operator<(const Relator& lhs, const Relator& rhs) {
-    return lhs.letters < rhs.letters;
-  }
-  bool operator==(const Relator& lhs, const Relator& rhs) {
-    return lhs.letters == rhs.letters;
-  }
-  std::vector<LetterType>::const_iterator Relator::begin() const {
-    return letters.begin();
-  }
-  std::vector<LetterType>::const_iterator Relator::end() const {
-    return letters.end();
-  }
-  void Relator::insert(std::vector<LetterType>::const_iterator position,
-	      std::vector<LetterType>::const_iterator first,
-	      std::vector<LetterType>::const_iterator last) {
-    letters.insert(position, first, last);
-  }
+void Relator::push_back(LetterType x) {
+    _letters.push_back(x);
 }
+
+size_t Relator::size() const {
+    return _letters.size();
+}
+    
+void Relator::reserve(size_t n) {
+    _letters.reserve(n);
+}
+    
+LetterType Relator::back() const {
+    return _letters.back();
+}
+
+LetterType &Relator::operator[](int index) {
+    return _letters[index];
+}
+
+LetterType Relator::operator[](int index) const {
+    return _letters[index];
+}
+    
+std::vector<LetterType>::const_iterator Relator::begin() const {
+    return _letters.begin();
+}
+
+std::vector<LetterType>::const_iterator Relator::end() const {
+    return _letters.end();
+}
+    
+bool operator<(const Relator& lhs, const Relator& rhs) {
+    return lhs._letters < rhs._letters;
+}
+
+bool operator==(const Relator& lhs, const Relator& rhs) {
+    return lhs._letters == rhs._letters;
+}
+
+}
+
--- cpp_low_index/cpp_src/relator.h
+++ cpp_low_index/cpp_src/relator.h
@@ -1,33 +1,40 @@
 #ifndef LOW_INDEX_RELATOR_H
 #define LOW_INDEX_RELATOR_H
 
-#include <vector>
-#include <iterator>
 #include "types.h"
-#include "relator.h"
+
+#include <vector>
 
 namespace low_index {
 
-  class Relator {
-  private:
-    std::vector<LetterType> letters;
-  public:
+class Relator
+{
+public:
     void push_back(LetterType x);    
     void reserve(size_t n);
-    size_t size();
     size_t size() const;
-    LetterType back();
     LetterType back() const;
-    LetterType operator[](int);
+    LetterType &operator[](int);
     LetterType operator[](int) const;
-    friend bool operator<(const Relator& lhs, const Relator& rhs);
-    friend bool operator==(const Relator& lhs, const Relator& rhs);
     std::vector<LetterType>::const_iterator begin() const;
     std::vector<LetterType>::const_iterator end() const;
+    template <class InputIterator>
     void insert(std::vector<LetterType>::const_iterator position,
-		std::vector<LetterType>::const_iterator first,
-		std::vector<LetterType>::const_iterator last);
-  };
+                InputIterator first,
+                InputIterator last)
+    {
+        _letters.insert(position, first, last);
+    }
+
+private:
+    friend bool operator<(const Relator& lhs, const Relator& rhs);
+    friend bool operator==(const Relator& lhs, const Relator& rhs);
+
+    std::vector<LetterType> _letters;
+};
+
+bool operator<(const Relator& lhs, const Relator& rhs);
+bool operator==(const Relator& lhs, const Relator& rhs);
 
 }
 #endif
